c74a0375a8aff1ce89309eea59bea76aace2b312
GrantK <grantklees108@gmail.com>
2025-01-21 14:16:12 -0800

    Fixed IncreasedAIWalkSpeed

--- Scripts/modules/IncreasedAiWalkSpeed.lua
+++ Scripts/modules/IncreasedAiWalkSpeed.lua
@@ -3,20 +3,22 @@ local config = require("config")
 
 
 if config.ModsEnabled.FastWalkSpeed then
-    local function OnSpawn(player)
-        local ServerWalkSpeed = player:GetPropertyValue("CharacterMovement")
-        if ServerWalkSpeed then
-            ServerWalkSpeed:SetPropertyValue("MaxWalkSpeed", config.SpeedSettings.WalkSpeed)
-        end
-    end
-    NotifyOnNewObject('/Game/Blueprints/Characters/Player/AI_Server/BP_Player_AI_Server.BP_Player_AI_Server_C', OnSpawn)
-	local function OnSpawn(player)
-        local BusserWalkSpeed = player:GetPropertyValue("CharacterMovement")
-        if BusserWalkSpeed then
-           BusserWalkSpeed:SetPropertyValue("MaxWalkSpeed", config.SpeedSettings.WalkSpeed)
-        end
-    end
-    NotifyOnNewObject('/Game/Blueprints/Characters/Player/AI_Busser/BP_Player_AI_Busser.BP_Player_AI_Busser_C', OnSpawn)
+	local function setServerWalkingSpeed()
+			local findAllAI = FindAllOf("CharacterMovementComponent")
+			local busserPattern = "CharacterMovementComponent /Game/Levels/BakeryLevel_World%.BakeryLevel_World:PersistentLevel%.BP_Player_AI_Busser_C_%d+%.CharMoveComp"
+			local serverPattern = "CharacterMovementComponent /Game/Levels/BakeryLevel_World%.BakeryLevel_World:PersistentLevel%.BP_Player_AI_Server_C_%d+%.CharMoveComp"
+			for _, aiCharacter in pairs(findAllAI) do
+				local fullName = aiCharacter:GetFullName()
+				if(debuggingPrints)
+					print(fullName)
+				end
+				if fullName:match(busserPattern) or fullName:match(serverPattern) then
+					aiCharacter:SetPropertyValue("MaxWalkSpeed", config.SpeedSettings.WalkSpeed)
+				end
+			end
+
+	end
+	setServerWalkingSpeed()  
 end
 if config.debug.listEnabledModules then
 	print("Increased Ai Walk Speed Loaded Module Loaded")
